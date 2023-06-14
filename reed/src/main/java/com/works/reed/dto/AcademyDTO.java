ab88f1c10fe6ecb6386b9a430b1f10d204168980
QAQA0 <qaqa061103@gmail.com>
2023-06-14 18:01:00 +0900

    학원 작업 코드 저장용 커밋

--- reed/src/main/java/com/works/reed/dto/AcademyDTO.java
+++ reed/src/main/java/com/works/reed/dto/AcademyDTO.java
@@ -1,5 +1,7 @@
 package com.works.reed.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.works.reed.entity.MemberEntity;
 import lombok.*;
 
@@ -8,9 +10,18 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AcademyDTO {
+    @JsonProperty("ACA_ASNUM")
     private Long ACA_ASNUM;
+    @JsonProperty("ATPT_OFCDC_SC_CODE")
     private String ATPT_OFCDC_SC_CODE;
+    @JsonProperty("ACA_NM")
     private String ACA_NM;
+    @JsonProperty("FA_RDNMA")
+    private String FA_RDNMA;
+    @JsonProperty("FA_RDNDA")
+    private String FA_RDNDA;
     private MemberEntity member;
+
 }
