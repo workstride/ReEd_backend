8525a4ab14e6f6e0970cb1ee4ba510f1d118b147
최현호 <117135776+QAQA0@users.noreply.github.com>
2023-11-02 14:06:01 +0900

    Update MailController.java
    swagger 배포용

--- reed/src/main/java/com/works/reed/domain/mail/api/MailController.java
+++ reed/src/main/java/com/works/reed/domain/mail/api/MailController.java
@@ -1,6 +1,7 @@
 package com.works.reed.domain.mail.api;
 
 import com.works.reed.domain.mail.application.MailService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +22,14 @@ public class MailController {
 
     private final MailService mailService;
 
+    @Operation(summary = "인증 코드 전송", description = "인증 코드 전송")
     @PostMapping("/send")
     public void mailSending(@RequestBody Map<String, String> mailMap) throws Exception {
         String mail = mailMap.get("mail");
         mailService.sendMail(mail);
     }
 
+    @Operation(summary = "인증 코드 등록", description = "인증 코드 등록")
     @GetMapping("/verify")
     public ResponseEntity mailVerify(@RequestBody Map<String, String> mailMap) {
         String mail = mailMap.get("mail");
